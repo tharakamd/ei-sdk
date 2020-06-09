02786196644bac705e0f5bcfe24eb41748ba74d0
Dilan Tharaka <tharakamd6@gmail.com>
2020-06-09 09:36:00 +0530

    add initial project code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# mi-apk
+# Enterprise Integrator SDK
+
---
+++ src/main/java/org/wso2/mi/apk/SimpleMediator.java
@@ -0,0 +1,51 @@
+package org.wso2.mi.apk;
+
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.SynapseLog;
+import org.apache.synapse.mediators.AbstractMediator;
+
+/**
+ * A simplified version of AbstractMediator. This can be used to create new mediator classes to run in Class mediator
+ * . AbstractExtendedMediator provides an abstract mediate method which provides a SimpleMessageContext object, which
+ * can be used to perform data transformation tasks easily
+ */
+public abstract class SimpleMediator extends AbstractMediator {
+
+    protected SimpleMediator() {
+
+        super();
+    }
+
+    @Override
+    public final boolean mediate(MessageContext messageContext) {
+
+        try {
+            mediate(new SimpleMessageContext(messageContext));
+            return true;
+        } catch (RuntimeException e) {
+            getLog(messageContext).error(e);
+            return false;
+        }
+    }
+
+    /**
+     * Mediate method with more capabilities than the parent method. No need to return boolean feedback when there is
+     * an error. Error tracking is done by an overloaded method.
+     * Throw SimpleMessageContextException to stop mediation flow due to an error.
+     *
+     * @param messageContext SimpleMessageContext instance which provides methods for work with payloads and data
+     *                       transformation.
+     */
+    public abstract void mediate(SimpleMessageContext messageContext);
+
+    /**
+     * Get a SynapseLog instance appropriate for the given context.
+     *
+     * @param simpleMessageContext current SimpleMessageContext
+     * @return MediatorLog instance - an implementation of the SynapseLog
+     */
+    protected SynapseLog getLog(SimpleMessageContext simpleMessageContext) {
+
+        return super.getLog(simpleMessageContext.getMessageContext());
+    }
+}
