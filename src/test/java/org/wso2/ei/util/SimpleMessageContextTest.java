dd5445136107da3104fef3b473ae36ae6713e47b
Dilan Tharaka <tharakamd6@gmail.com>
2020-06-19 10:35:16 +0530

    test cases

--- src/test/java/org/wso2/ei/util/SimpleMessageContextTest.java
+++ src/test/java/org/wso2/ei/util/SimpleMessageContextTest.java
@@ -1,5 +1,40 @@
 package org.wso2.ei.util;
 
-class SimpleMessageContextTest {
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.commons.json.JsonUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.testng.PowerMockTestCase;
+import org.testng.annotations.Test;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@PrepareForTest({JsonUtil.class})
+class SimpleMessageContextTest extends PowerMockTestCase {
+
+    @Mock
+    private MessageContext messageContextMock;
+    private SimpleMessageContext simpleMessageContext;
+
+    @BeforeEach
+    void setUp() {
+
+        simpleMessageContext = new SimpleMessageContext(messageContextMock);
+    }
+
+    @Test
+    public void getJsonStringTest() {
+
+        mockStatic(JsonUtil.class);
+        when(JsonUtil.jsonPayloadToString(any())).thenReturn("a");
+
+        final String result = simpleMessageContext.getJsonString();
+
+        Assertions.assertEquals("a", result);
+
+    }
 }
